246ecff23fc77518841bfbd4e0720f8866531738
Mateo Álvarez <maalvarezmu@unal.edu.co>
2023-10-15 21:01:59 -0500

    Configuración base

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-import './style.css';
-import javascriptLogo from './javascript.svg';
-import viteLogo from './dist/vite.svg';
+import '../css/style.css';
+import javascriptLogo from '../assets/javascript.svg';
+import viteLogo from '../dist/vite.svg';
 import setupCounter from './counter';
 
 document.querySelector('#app').innerHTML = `
