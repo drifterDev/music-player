87f4cab5ce7c8c4c308dcdb6ebde8d306458872c
Mateo Álvarez <maalvarezmu@unal.edu.co>
2023-10-16 18:32:08 -0500

    Conectando el html y el js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
 import '../css/style.css';
+
+const buttonPlay = document.getElementById('play');
+const labelPlay = document.getElementById('label-play');
+const nextButton = document.getElementById('next');
+const prevButton = document.getElementById('prev');
+
+function changeSong() {
+  console.log('change song');
+}
+
+nextButton.addEventListener('click', () => {
+  changeSong();
+});
+
+prevButton.addEventListener('click', () => {
+  changeSong();
+});
+
+buttonPlay.addEventListener('click', () => {
+  labelPlay.classList.toggle('fa-pause');
+  labelPlay.classList.toggle('fa-play');
+});
