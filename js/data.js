17484f5af67e550bd895b5d6eb28b274eb5c9f2f
Mateo Álvarez <maalvarezmu@unal.edu.co>
2023-10-16 19:55:09 -0500

    Interactividad

---
+++ js/data.js
@@ -0,0 +1,15 @@
+/* eslint-disable linebreak-style */
+const data = {
+  1: {
+    title: 'Lost in the City Lights',
+    author: 'Cosmo Sheldrake',
+    src: 'lost-in-city-lights-145038.mp3',
+  },
+  2: {
+    title: 'Forest Lullaby',
+    author: 'Lesfm',
+    src: 'forest-lullaby-110624.mp3',
+  },
+};
+
+export default data;
--- js/main.js
+++ js/main.js
@@ -1,20 +1,41 @@
 import '../css/style.css';
+import data from './data';
 
+const audio = document.getElementById('audio');
 const buttonPlay = document.getElementById('play');
 const labelPlay = document.getElementById('label-play');
 const nextButton = document.getElementById('next');
 const prevButton = document.getElementById('prev');
+const imgCover = document.getElementById('cover');
+const authorLabel = document.getElementById('author');
+const titleLabel = document.getElementById('title');
+const limitSongs = Object.keys(data).length;
+let songActive = 1;
 
-function changeSong() {
-  console.log('change song');
+function changeSong(option) {
+  if (option === 'next') {
+    songActive += 1;
+    if (songActive > limitSongs) {
+      songActive = 1;
+    }
+  } else {
+    songActive -= 1;
+    if (songActive < 1) {
+      songActive = limitSongs;
+    }
+  }
+  imgCover.src = `./assets/cover-${songActive}.png`;
+  authorLabel.innerHTML = data[songActive].author;
+  titleLabel.innerHTML = data[songActive].title;
+  audio.src = `assets/${data[songActive].src}`;
 }
 
 nextButton.addEventListener('click', () => {
-  changeSong();
+  changeSong('next');
 });
 
 prevButton.addEventListener('click', () => {
-  changeSong();
+  changeSong('prev');
 });
 
 buttonPlay.addEventListener('click', () => {
